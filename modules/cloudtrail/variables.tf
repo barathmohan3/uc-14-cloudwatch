2d19d609cd74b608a64896256923790603cc6dff
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-23 14:39:03 +0530

    Update variables.tf

--- modules/cloudtrail/variables.tf
+++ modules/cloudtrail/variables.tf
@@ -1,4 +1,6 @@
 # modules/cloudtrail/variables.tf
 variable "name_prefix"   { type = string }
 variable "s3_bucket_name" { type = string }
-variable "log_retention_days" { type = number; default = 90 }
+variable "log_retention_days" 
+{ type = number
+ default = 90 }
