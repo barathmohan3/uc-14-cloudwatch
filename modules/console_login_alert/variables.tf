9a56d40cffe3a15a61b80695a053b55480d4b78e
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-23 14:41:47 +0530

    Update variables.tf

--- modules/console_login_alert/variables.tf
+++ modules/console_login_alert/variables.tf
@@ -1,6 +1,14 @@
 # modules/console_login_alert/variables.tf
 variable "prefix"           { type = string }
 variable "log_group_name"   { type = string }
-variable "metric_namespace" { type = string; default = "Security/CloudTrail" }
-variable "alarm_period_sec" { type = number; default = 300 }
+variable "metric_namespace" 
+{        
+      type = string
+      default = "Security/CloudTrail" 
+}
+variable "alarm_period_sec" 
+{ 
+  type = number
+  default = 300 
+}
 variable "email_address"    { type = string }
